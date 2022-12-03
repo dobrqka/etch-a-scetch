ef398475f0851a9b5830faab8be953fc1eb7cebe
dobrqka <dobrin.andreev@gmail.com>
2022-12-03 10:39:32 +0200

    Add coloring on click and while hovering
    Simple black coloring if you click on a div and a grey coloring
    leaving a trail when you hover over divs

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function setGrid () {
         newDiv.style.border = 'dotted black 1px';
         container.appendChild(newDiv);
     }
+    changeColor();
 }
 
 // button to create a grid according to a side size selection
@@ -23,3 +24,14 @@ const newGrid = document.createElement('button');
 newGrid.textContent = 'Create a grid';
 document.body.appendChild(newGrid);
 newGrid.addEventListener('click', setGrid);
+
+// make the divs change color
+
+function changeColor() {
+    const boxes = document.querySelectorAll('.container div');
+    for (i=0; i<boxes.length; i++) {
+        boxes[i].addEventListener('mousedown', (e) => {e.target.classList.add('new-color')})
+        boxes[i].addEventListener('mouseenter', (e) => {e.target.classList.add('hover-color')})
+        boxes[i].addEventListener('mouseleave', (e) => {e.target.classList.remove('hover-color')})
+    }
+}
